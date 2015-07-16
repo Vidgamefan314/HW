fc285daafe4dc2ee0f6e7c861b3002388f4464d3
vidgamefan314 <tfarbs314@yahoo.com>
2015-07-16 02:16:20 +0000

    HW

--- jquery.js
+++ jquery.js
@@ -2,13 +2,13 @@ $( document ).ready(function() {
  
     $( "#xy" ).click(function() {
  
-       $( "#one, #two, #three, #ax, #to, #ty" ).toggleClass("ef");
+       $( "#one, #two, #three, #ax, #to, #ty, #mk" ).toggleClass("ef");
  
 });event;
 $( document ).ready(function() {
   $( "#two" ).hover(function() {
     
-  $( "#two" ).replaceWith("<iframe id='mk' src='https://www.youtube.com/embed/dQw4w9WgXcQ'?frameborder='0'&allowfullscreen&autoplay></iframe>")
+  $( "#two" ).replaceWith("<iframe id='mk' src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0'  allowfullscreen autoplay></iframe>");
 });
 });
 });
